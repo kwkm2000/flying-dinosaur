e04be5c10d8062f651111c6e822b65e0629c212c
r-kawakami <r-kawakami@mac156.local>
2020-11-13 10:35:56 +0900

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public Rigidbody2D Body { get; private set; }
+    public RectTransform RectTransform { get; private set; }
+
+    void Awake() {
+        this.Body = gameObject.GetComponent<Rigidbody2D>();
+        this.RectTransform = gameObject.GetComponent<RectTransform>();
+    }
+
+    void Update() {
+        var currentX = this.RectTransform.localPosition.x;
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Debug.Log("Right");
+            var newX = Mathf.Min(currentX + 15, 230);
+            var pos = new Vector3(newX, this.RectTransform.localPosition.y, 0);
+            Debug.Log(pos);
+            this.RectTransform.localPosition = pos;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)) {
+            Debug.Log("Left");
+
+            var newX = Mathf.Max(currentX - 15, -230);
+            var pos = new Vector3(newX, this.RectTransform.localPosition.y, 0);
+            this.RectTransform.localPosition = pos;
+        }
+    }
+}
