2062cb415ff7c9cc5149525572fe99a401801ff4
kwkm2000 <r-kawakami@mac156.local>
2020-11-19 20:42:49 +0900

    Scoreを加算

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+	public Text scoreText;
+	private int score = 0;
+
+	void Update()
+    {
+		score += 1;
+		scoreText.text = "SCORE: " + score.ToString();
+	}
+}
