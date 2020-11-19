2baefbe495365352fec8b0e1f06cfce5c9e01b64
kwkm2000 <r-kawakami@mac156.local>
2020-11-20 00:28:38 +0900

    リトライボタンの実装

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    private bool firstPush = false;
+    public void PressRetry()
+    {
+        if (!firstPush)
+        {
+            SceneManager.LoadScene("Main", LoadSceneMode.Single);
+            firstPush = true;
+        }
+    }
+}
